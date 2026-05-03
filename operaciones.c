2e787e7603b89ba9fab9680ababdd9092e2f8ad7
matias rajmilevich <matiasgrajmilevich@gmail.com>
2026-05-03 14:03:07 -0300

    estaban los heads

--- operaciones.c
+++ operaciones.c
@@ -2,11 +2,7 @@
 
 /* ── sumar — ya implementada, referencia de estilo ───────────────────────── */
 
-<<<<<<< HEAD
-int sumar(const int a, const int b) {
-=======
 int sumar(int a, int b) {
->>>>>>> origin/feature/conflicto-demo
     return a + b;
 }
 
